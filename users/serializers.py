7c1f6dab2dac52a4d014ce01cb2637e39b88be89
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-30 01:23:10 +0300

    adds the id field to the CustomUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
+from habits.serializers import HabitSerializer
 from users.models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('email', 'phone', 'telegram', 'avatar',)
+        fields = ('id', 'email', 'phone', 'telegram', 'avatar', 'last_login')
+
+class CustomUserDetailSerializer(serializers.ModelSerializer):
+    user_habits = HabitSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'email', 'phone', 'telegram', 'avatar', 'last_login', 'user_habits')

efa43c34cb6402a8b1002c6e737608f6834f0c0e
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-29 18:21:18 +0300

    adds each model to the admin panel

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit, AuxiliaryHabit, Reward
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'action', 'action_time', 'action_place', 'duration', 'regularity', 'is_public',)
+
+
+@admin.register(AuxiliaryHabit)
+class AuxiliaryHabitAdmin(admin.ModelAdmin):
+    list_display = ('main_habit', 'name', 'action', 'action_time', 'action_place', 'duration', 'is_public',)
+
+
+@admin.register(Reward)
+class RewardHabitAdmin(admin.ModelAdmin):
+    list_display = ('main_habit', 'name', 'description', 'resources',)

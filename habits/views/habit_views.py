ea0cfaaa42105fd87a493b8c497895f30895d155
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-30 01:56:00 +0300

    adds self.request.user assignment for new habit objects

--- habits/views/habit_views.py
+++ habits/views/habit_views.py
@@ -7,14 +7,21 @@ from habits.serializers import HabitSerializer
 class HabitCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
 
+    def perform_create(self, serializer):
+        new_habit = serializer.save()
+        new_habit.user = self.request.user
+        new_habit.save()
+
 
 class HabitListAPIView(generics.ListAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
 
+
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     queryset = Habit.objects.all()

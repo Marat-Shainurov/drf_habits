9feabe5f0a183c74807008f8da671d2ef00448a2
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-30 02:11:39 +0300

    sets authentication

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework_simplejwt',
+
     'users',
     'habits',
 ]
@@ -132,3 +135,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'],
+}
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-
 from users.apps import UsersConfig
 from users.views import CustomUserListAPIView, CustomUserCreateAPIView, CustomUserUpdateAPIView, \
     CustomUserDestroyAPIView, CustomUserRetrieveAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', CustomUserListAPIView.as_view(), name='users_list'),
     path('users/detail/<int:pk>/', CustomUserRetrieveAPIView.as_view(), name='users_detail'),
     path('users/create/', CustomUserCreateAPIView.as_view(), name='users_create'),

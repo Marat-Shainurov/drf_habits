27426acd0ce451a943eb0093ad191dccb9ce84d5
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-29 17:00:31 +0300

    updates init file in the habits/models package

--- habits/models/__init__.py
+++ habits/models/__init__.py
@@ -2,4 +2,4 @@ from .habit import Habit
 from .auxiliary_habit import AuxiliaryHabit
 from .reward import Reward
 
-__all__ = ['Habit', 'AuxiliaryHabit', 'Reward',]
+__all__ = ['Habit', 'AuxiliaryHabit', 'Reward', ]

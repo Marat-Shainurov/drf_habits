59325aa5509a9ffacbcdb70bf594920744e9287e
Marat Shainurov <m_shaynurov@outlook.com>
2023-07-31 18:34:56 +0300

    updates the CustomUser model with the chat_id field

--- users/admin.py
+++ users/admin.py
@@ -6,4 +6,4 @@ from users.models import CustomUser
 # Register your models here.
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email',)
+    list_display = ('email', 'phone', 'telegram', 'avatar')
